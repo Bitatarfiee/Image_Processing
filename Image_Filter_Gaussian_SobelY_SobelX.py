21ee6e0eaaed4a18c91d2670226ff289b309b052
Bita <b.tarfiee@gmail.com>
2024-11-03 14:51:36 +0100

    Adding plot option for three flitered images

--- Image_Filter_Gaussian_SobelY_SobelX.py
+++ Image_Filter_Gaussian_SobelY_SobelX.py
@@ -40,3 +40,20 @@ filterResponseGauss = signal.convolve2d(image, gaussFilter, mode='same')
 filterResponseSobelX = signal.convolve2d(image, sobelX, mode='same')
 filterResponseSobelY = signal.convolve2d(image, sobelY, mode='same')
 
+import matplotlib.pyplot as plt
+
+# Show filter responses
+fig, (ax_orig, ax_filt1, ax_filt2, ax_filt3) = plt.subplots(1, 4, figsize=(20, 6))
+ax_orig.imshow(image, cmap='gray')
+ax_orig.set_title('Original')
+ax_orig.set_axis_off()
+ax_filt1.imshow(np.absolute(filterResponseGauss), cmap='gray')
+ax_filt1.set_title('filterResponseGauss')
+ax_filt1.set_axis_off()
+ax_filt2.imshow(np.absolute(filterResponseSobelX), cmap='gray')
+ax_filt2.set_title('filterResponseSobelX')
+ax_filt2.set_axis_off()
+ax_filt3.imshow(np.absolute(filterResponseSobelY), cmap='gray')
+ax_filt3.set_title('filterResponseSobelY')
+ax_filt3.set_axis_off()
+plt.imsave('filterResponseimage.png', image)
